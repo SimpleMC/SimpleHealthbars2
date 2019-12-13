31dbc0b22d845a6de1d68e458738dbb3354aa18d
Taylor Becker <taylor.becker@triwavestudios.com>
2019-12-13 17:31:21 -0500

    Add axion release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,32 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import pl.allegro.tech.build.axion.release.domain.hooks.HookContext
+import pl.allegro.tech.build.axion.release.domain.hooks.HooksConfig
 
 plugins {
     kotlin("jvm") version "1.3.61"
     id("com.github.johnrengelman.shadow") version "5.1.0"
+    id("pl.allegro.tech.build.axion-release") version "1.10.2"
 }
 
+val mcApiVersion = "1.15"
+
 group = "org.simplemc"
-version = "1.15-SNAPSHOT"
+version = scmVersion.version
+
+scmVersion {
+    hooks(closureOf<HooksConfig> {
+        pre(
+            "fileUpdate",
+            mapOf(
+                "file" to "src/main/resources/plugin.yml",
+                "pattern" to KotlinClosure2<String, HookContext, String>({ v, _ -> "version: $v\\napi-version: \".+\"" }),
+                "replacement" to KotlinClosure2<String, HookContext, String>({ v, _ -> "version: $v\napi-version: \"$mcApiVersion\"" })
+            )
+        )
+        pre("commit")
+    })
+}
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -22,7 +41,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    compileOnly(group = "org.spigotmc", name = "spigot-api", version = "1.15+")
+    compileOnly(group = "org.spigotmc", name = "spigot-api", version = "$mcApiVersion+")
 }
 
 tasks {
